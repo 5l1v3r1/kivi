b1be01f955f61616960ca2833d7ccbb3993e9c15
furkanonder <furkantahaonder@gmail.com>
2020-04-21 00:38:34 +0300

    update setup conf

--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
-# python
+from pathlib import Path
 from setuptools import setup
-import os
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+current_dir = Path(__file__).parent.resolve()
 
+with open(current_dir / 'README.md', encoding='utf-8') as f:
+    long_description = f.read()
+    
 setup(
     name="kivi",
     version = "0.0.1",
     packages = ["kivi"],
     description = "It calculates the creation time and size of objects.",
     author = "furkanonder",
-    author_email = "furkantahaonder@gmail.com",
-    url = "https://www.github.com/furkanonder/kivi",
-    python_requires='>=3',
-    py_modules=[],
-    long_description=read('README.md'),
-    include_package_data = True,
-    install_requires = [],
+    author_email = 'furkanonder@protonmail.com',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
     keywords = ["class", "time", "size", "objects"],
     license = "MIT",
     classifiers=[
@@ -26,4 +23,3 @@ setup(
         'Programming Language :: Python :: 3.5',
     ],
 )
-
